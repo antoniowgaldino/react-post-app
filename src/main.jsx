455edc43dcd661be71875114a534b3c28fda1536
ANTONIO ALBERTO WOWTILLA E SILVA GALD <ANTONIO9661@DIGITALCOLLEGE.LOCAL>
2024-11-07 19:10:01 -0300

    instalando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { login } from './pages/login/login.jsx'
+import { home } from './pages/home/home.jsx'
+import { post } from './pages/post/post.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/post",
+    element: <Post />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
